afe9a22b19da0da2bc0e5dc653d544ac932fd030
yuaner <873064448@qq.com>
2021-10-29 15:56:14 +0800

    10.29

--- 自动化测试框架/多线程自动化框架/InitPage.py
+++ 自动化测试框架/多线程自动化框架/InitPage.py
@@ -1,11 +1,11 @@
-class case:
-    login_success_data = [
-        {"username": "jason", "passwork": "1234567", "expect": "Student Login"},
-        {"username": "admin", "passwork": "root", "expect": "Student Login"},
-        {"username": "jason", "passwork": "root", "expect": "Student Login12345"}
+class InitPage:
+    login_success_date = [
+        {"username": "jason", "pwd": "1234567", "expect": "Student Login"},
+        {"username": "admin", "pwd": "root", "expect": "Student Login12345"}
     ]
 
     login_error_data = [
-        {"username": "jason0", "passwork": "1234567", "expect": "账户名错误或密码错误!别瞎弄!"},
-        {"username": "jason", "passwork": "root0", "expect": "账户名错误或密码错误!别瞎弄!"}
+        #  id:msg_uname
+        {"username": "jason1", "pwd": "1234567", "expect": "账户名错误或密码错误!别瞎弄!"},
+        {"username": "admin", "pwd": "root1", "expect": "账户名错误或密码错误!别瞎弄!"}
     ]
